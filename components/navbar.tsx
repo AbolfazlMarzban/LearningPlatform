fc59181e227964050748b34afafaf3dffcbabb8f
Abolfazl <abolfazlmarzban39@gmail.com>
2023-06-20 21:19:46 +0430

    abol 1402-3-30

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import logo from '../public/logo-abol.png'
 
 function Navbar() {
     return (
-        <div className="h-28">
+        <div className="h-16">
             <Image src={logo} alt={'abolfazlmarzban'} className="h-4/5 w-auto"></Image>
         </div>
     );
