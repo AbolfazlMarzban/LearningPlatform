53010c4967acd7e94dd93c033a0d40692d8fb09f
Abolfazl <abolfazlmarzban39@gmail.com>
2023-06-20 23:45:49 +0430

    abol 1402-3-30

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,14 +3,17 @@ import logo from '../public/logo-abol.png'
 
 function Navbar() {
     return (
-        <div className="h-16 flex flex-row items-center justify-between w-full">
-            <div className="w-32">
+        <div className="h-16 flex flex-row items-center justify-between w-full px-2">
+            <div className="w-40">
             <Image src={logo} alt={'abolfazlmarzban'} className="h-4/5 w-auto"></Image>
             </div>
             <div>
-                <ul >
+                <ul className="flex flex-row gap-x-10">
+                    <li>Services</li>
                     <li>Projects</li>
                     <li>Courses</li>
+                    <li>About me</li>
+                    <li>Let's connect</li>
                 </ul>
             </div>
         </div>
