a60f7bd19269ede94b962d059f09ded45a8b46e1
Abolfazl <abolfazlmarzban39@gmail.com>
2023-06-21 09:26:16 +0430

    abol 1402-3-31

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import { Noto_Sans_Mono } from 'next/font/google'
 import Navbar from './navbar'
+import Footer from './footer';
 
 
 const noto = Noto_Sans_Mono({ subsets: ['latin']})
@@ -7,10 +8,11 @@ const noto = Noto_Sans_Mono({ subsets: ['latin']})
 function Layout({children} : any) {
     return (
         <main
-        className={`flex min-h-screen flex-col items-center ${noto.className}`}
+        className={`flex min-h-screen flex-col justify-between items-center ${noto.className}`}
       >
         <Navbar></Navbar>
         {children}
+        <Footer></Footer>
       </main>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
   <Layout>
-    <div className="text-center">
-      <p className="font-bold text-8xl">This is</p>
-      <span className="font-extrabold text-9xl">Abolfazl Marzban</span>
+    <div className="text-center w-full">
+      <p className="font-bold text-5xl">This is</p>
+      <span className="font-extrabold text-7xl">Abolfazl Marzban</span>
     </div>
   </Layout>
   )
