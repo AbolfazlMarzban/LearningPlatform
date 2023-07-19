cb4c9b8045aac214cef2fc16ee4aa2c5dc26d8c8
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-07-19 18:13:39 +0330

    abol 1402-4-28

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,12 +1,24 @@
 import multiparty from "multiparty"
 import path from "path";
+import fs from "fs"
 
 export default async function handle(req: any, res:any){
     const form = new multiparty.Form();
     form.parse(req,  (err, fields, files)=> {
         if(err) throw err;
         console.log('files', files)
-        console.log('fields', fields)
+        for(var i=0; i< files.image.length; i++){
+            if(process.env.INIT_CWD){
+                var base = process.env.INIT_CWD.toString()
+                var newPath = path.join(base, "public", "uploads", files.image[i].originalFilename)
+                console.log('newPath', newPath)
+                      fs.rename(files.image[i].path, newPath, (err)=>{
+                throw err;
+            })
+            }
+
+      
+        }
         res.json('ok')
     })
 }
