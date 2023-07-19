c36755d93ae92d33fd8622a9d8b296f3551cc06b
Abolfazl <abolfazlmarzban39@gmail.com>
2023-07-19 10:09:53 +0430

    abol 1402-4-28

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,9 +1,13 @@
 import multiparty from "multiparty"
+import path from "path";
 
 export default async function handle(req: any, res:any){
     const form = new multiparty.Form();
-    form.parse(req, async ()=> {
-        
+    form.parse(req,  (err, fields, files)=> {
+        if(err) throw err;
+        console.log('files', files)
+        console.log('fields', fields)
+        res.json('ok')
     })
 }
 
