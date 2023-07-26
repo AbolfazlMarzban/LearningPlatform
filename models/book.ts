3c2e0ec7259e6db945aa36dabe467ee089131e7c
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-07-26 15:44:41 +0330

    abol 1402-5-4

---
+++ models/book.ts
@@ -0,0 +1,11 @@
+const { Schema, model, models } = require("mongoose");
+
+const ProjectSchema = new Schema({
+    name:  {type: String, required: true },
+    // link: {type: String, required: true },
+    description: { type: String, required: true },
+    address: { type: String, required: true },
+    price: { type: Number, required: true}
+})
+
+export const Project = models.Project || model('Project', ProjectSchema)
