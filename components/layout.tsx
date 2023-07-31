1c86c291d1daa6ff93b412efa7a6a6e8dcfd8036
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-07-31 11:17:52 +0330

    abol 1402-5-9

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,37 @@
 import { Noto_Sans_Mono } from 'next/font/google'
 import Navbar from './navbar'
 import Footer from './footer';
-
+import { useEffect, useRef, useState } from 'react';
+import * as THREE from "three"
+// @ts-ignore
+import NET from "vanta/dist/vanta.net.min"
 
 const noto = Noto_Sans_Mono({ subsets: ['latin']})
 
 function Layout({children} : any) {
-  
+// const NET : any = require("vanta/dist/vanta.globe.min")
+const [vantaEffect, setVantaEffect] : any = useState(0)
+const vantaRef = useRef(null)
+useEffect(()=>{
+if(!vantaEffect){
+  setVantaEffect(
+    NET({
+      el: vantaRef.current,
+      THREE,
+      color: 0x0,
+      backgroundColor: 0xc5c5c5,
+      showDots: false
+    })
+  )
+}
+return ()=>{
+  if (vantaEffect) vantaEffect.destroy()
+}
+}, [vantaEffect])
     return (
         <main
         className={`flex min-h-screen flex-col justify-between items-center ${noto.className}`}
+        ref={vantaRef}
       >
         <Navbar></Navbar>
         {children}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      {/* <script src="three.r134.min.js"></script>
-<script src="vanta.clouds.min.js"></script> */}
       </Head>
       <body>
         <Main />
