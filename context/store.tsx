a84ef923104678d02188fbff152e11a08044e2aa
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-02 15:00:11 +0330

    abol 1402-5-11

---
+++ context/store.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { createContext, useContext, Dispatch, SetStateAction, useState } from "react";
+
+type DataType = {
+    firstName: string
+}
+
+interface ContextProps {
+    userId: string,
+    setUserId: Dispatch<SetStateAction<string>>,
+    data: DataType[],
+    setData: Dispatch<SetStateAction<DataType[]>>
+}
+
+const GlobalContext = createContext<ContextProps>({
+    userId: '',
+    setUserId: (): string => '',
+    data: [],
+    setData: (): DataType[] => []
+})
+
+
+export const GlobalContextProvider = ({children}: any) => {
+    const [userId, setUserId] = useState('')
+    const [data, setData] = useState<[] | DataType[]>([])
+
+}
