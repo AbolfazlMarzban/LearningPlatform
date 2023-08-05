1c4d95b7f91fbb480f02039a1fb81561bff36815
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-05 14:43:07 +0330

    abol 1402-5-14

---
+++ pages/user/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout"
+
+
+
+export default function User(){
+    return(
+        <Layout>
+            <div className="bg-white w-full flex flex-row">
+
+            </div>
+        </Layout>
+    )
+}
