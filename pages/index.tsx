76d3637df2a589c9ea5c69ad5d476e0bed1b07a4
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-14 15:52:49 +0330

    abol 1402-5-23

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
   <Layout>
-    
+    <div className="w-full">
+    <h2>Courses</h2>
+    <hr />
+    <h2>Books</h2>
+    <hr />
+    </div>
   </Layout>
   )
 }
