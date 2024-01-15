aa19811f117710898509102dde101a7a2244f9d4
Abolfazl <abolfazlmarzban39@gmail.com>
2024-01-15 12:37:31 +0330

    abol sign out

--- components/adminLayout.tsx
+++ components/adminLayout.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { Noto_Sans_Mono } from 'next/font/google'
 import Navbar from './navbar';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const noto = Noto_Sans_Mono({ subsets: ['latin']})
 
 function AdminLayout({children}:any) {
+    const router = useRouter()
+    function exitUser(){
+        localStorage.removeItem('abolID')
+        router.push('/')
+    }
     return (
         <main
         className={`flex min-h-screen flex-col justify-between items-center bg-white ${noto.className}`}
@@ -32,6 +38,9 @@ function AdminLayout({children}:any) {
                 <Link href={'/admin/contacts'} className='p-4 hover:bg-white hover:text-sky-900'>
                     Contacts
                 </Link>
+                <span className='p-4 hover:bg-white hover:text-sky-900 cursor-pointer' onClick={()=>exitUser()}>
+                    Sign Out
+                </span>
                 </nav>
             </aside>
             <div className='w-full p-4 border-solid border-4 border-sky-900'>
