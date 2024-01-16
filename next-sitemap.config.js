e6610f91523072426b30a13e71a34f6919b7c622
Abolfazl <abolfazlmarzban39@gmail.com>
2024-01-16 16:15:13 +0330

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,5 @@ const siteUrl = 'https://abolfazlmarzban.netlify.app'
 module.exports= {
     siteUrl,
     generateRobotsTxt: true,
-    exclude: ['/admin','/admin/**'],
+    exclude: ['/courses','/books','/cart','/user','/login','/admin','/admin/**'],
 }
