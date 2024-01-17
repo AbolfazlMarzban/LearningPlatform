f9fdc0141209c639f6b7eb9f42d155fefde27e9d
Abolfazl <abolfazlmarzban39@gmail.com>
2024-01-17 11:52:50 +0330

    postform

---
+++ pages/admin/blog/newPost.tsx
@@ -0,0 +1,13 @@
+import AdminLayout from '@/components/adminLayout';
+import PostForm from '@/components/postForm';
+
+function NewPost() {
+  return (
+    <AdminLayout>
+        <PostForm />
+    </AdminLayout>
+        
+  );
+}
+
+export default NewPost;
