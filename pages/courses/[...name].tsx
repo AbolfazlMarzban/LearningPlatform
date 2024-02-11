c35b40082b73ab427be10426c1f34ad116e99e43
Abolfazl <abolfazlmarzban39@gmail.com>
2024-02-11 16:43:02 +0330

    fixed

--- pages/courses/[...name].tsx
+++ pages/courses/[...name].tsx
@@ -1,16 +1,23 @@
-'use client';
-
 import Layout from "@/components/layout";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import CourseSlug from "@/components/courseSlug";
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
+    return {
+        paths:[],
+        fallback: true
+    }
+}
+
+export async function getStaticProps({params}:any) {
+    const {slug} = params
     const footer = await fetch(`${process.env.BASE_URL}/api/contactsManage`, {
       method: "GET",
     });
     const data = await footer.json();
+    console.log('footer', data)
     return {
       props: {
         footer: data[0],
@@ -22,20 +29,23 @@ export default function Course({footer}:any){
     const router = useRouter()
     var name :any = router.query.name
     const [courseInfo, setCourseInfo] : any = useState({})
-    useEffect(()=>{
-        if(name){
-            axios.get('/api/courses?name=' + name[0]).then(response =>{
-                console.log('course info', response.data)
-                setCourseInfo(response.data)
-            })
-        }
-    }, [name?.length])
+    // useEffect(()=>{
+    //     if(name){
+    //         axios.get('/api/courses?name=' + name[0]).then(response =>{
+    //             console.log('course info', response.data)
+    //             setCourseInfo(response.data)
+    //         })
+    //     }
+    // }, [name?.length])
     return(
-        <Layout footer={footer}>
-            {courseInfo && (
-            <CourseSlug 
-            {...courseInfo}/>
-            )}
-        </Layout>
+        // <Layout footer={footer}>
+        //     {courseInfo && (
+        //     <CourseSlug 
+        //     {...courseInfo}/>
+        //     )}
+        // </Layout>
+        <div>
+            
+        </div>
     )
 }
