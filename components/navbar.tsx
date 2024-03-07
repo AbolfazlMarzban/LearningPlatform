fcc667ff2aeead18178191b1665e3e1ec6c61104
Abolfazl <abolfazlmarzban39@gmail.com>
2024-03-07 09:06:10 -0800

    no recoil

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,8 +5,6 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { cartState } from "@/context/store";
-import {useRecoilState} from "recoil"
 
 
 const navigation = [
@@ -24,8 +22,6 @@ function classNames(...classes: any) {
 }
 
 function Navbar() {
-
-  const [cartItems] = useRecoilState(cartState)
   return (
     <Disclosure as="nav" className="w-full">
       {({ open }) => (
@@ -88,7 +84,7 @@ function Navbar() {
                           d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                         />
                       </svg>
-                      <div className="absolute w-4 h-4 rounded-full -top-2 -right-2 bg-gray-400 text-white text-center text-xs">{cartItems.length}</div>
+                      <div className="absolute w-4 h-4 rounded-full -top-2 -right-2 bg-gray-400 text-white text-center text-xs">0</div>
                     </Link>
                     <Link href={"/user"}>
                       <svg
